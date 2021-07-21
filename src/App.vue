9f321d949b48480ba92a712834e9d9738d9600db
wvwall <waltervelardo@gmail.com>
2021-07-21 14:29:44 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="app">
+        <CustomTitle :titleText="message" :subtitleText="subMessage"/>
+        <img  class="poke" src="./assets/pokedex.png" alt="">
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+        <Pokemons />
+    </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import CustomTitle from "./components/CustomTitle.vue";
+import Pokemons from "./pages/Pokemons.vue";
+
+export default {
+    name: "App",
+    components: {
+        // HelloWorld
+        CustomTitle,
+        Pokemons,
+    },
+    data() {
+        return {
+            message: "Pokedex",
+            subMessage: "Giochiamo con i Pokemon"
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+@import './assets/scss/app.scss';
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    .poke {
+        width: 300px;
+        margin: 20px 0;
+    }
+}
+</style>
---
+++ src/components/CustomTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{ titleText }}</h1>
+        <h2>{{ subtitleText }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomTitle',
+    props: ['titleText', 'subtitleText']
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #F7C408;
+}
+</style>
